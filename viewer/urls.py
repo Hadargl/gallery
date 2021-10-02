5c99af0c051e86bd0dcc99f3e9a372bfd727fd9f
Harrison Long <harrisondgl2013@gmail.com>
2021-10-02 14:31:13 +0100

    added html

--- viewer/urls.py
+++ viewer/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.viewer_home, name='viewer-home'),
+    path('gallery1/', views.viewer_gallery_1, name='gallery-1'),
 ]
 
--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 def viewer_home(request):
     return render(request, 'viewer/home.html')
+
+def viewer_gallery_1(request):
+    return render(request, 'viewer/gallery1.html')
